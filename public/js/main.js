bc88746a473e13248b847325c2be0056ade2c465
Justin Van Voorst <justin.vanvoorst@gmail.com>
2016-07-15 16:51:12 -0600

    added validation

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,32 @@
-var app = angular.module('surveyApp', []);
+var app = angular.module('surveyApp', ['ngMessages']);
 
 app.controller('surveyCntrl', ['$scope', '$http', function($scope, $http) {
 
+    $scope.submitted = false;
     $scope.survey = {};
     $scope.survey.timeStamp = new Date();
 
     $scope.submitSurvey = function() {
 
-        console.log($scope.survey);
+        // set to true when submit button clicked
+        $scope.submitted = true;
 
-        $http.post('/api/submitSurvey', $scope.survey).then(function(res) {
-            if (res.data.error) {
-                //handle error
-                console.log(res.data);
-            }
-            else {
-                // redirect
-            }
-        });
+        if (form.location[0].validity.valid) {
+            console.log('can submit')
+        }
+        else {
+            console.log('cannot submit')
+        }
+
+        // $http.post('/api/submitSurvey', $scope.survey).then(function(res) {
+        //     if (res.data.error) {
+        //         //handle error
+        //         console.log(res.data);
+        //     }
+        //     else {
+        //         // redirect
+        //     }
+        // });
     };
 
 }]);
